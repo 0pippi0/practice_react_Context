09b2d0cf90a8dfe93646cb41abc94a29bb9d150c
0pippi0 <j.romany91@gmail.com>
2020-02-25 01:36:48 +0900

    배열의 내장함수 reduce 와 리액트 컴포넌트의 createElement 를 활용
    Context 갯수가 많아져도, Provider를 적용하기 위해서 코드의 구조가 깊어질 필요가 없답니다. contexts 를 props 로 전달만 해주면 되죠

--- context/src/App.js
+++ context/src/App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import LeftPane from './components/LeftPane';
 import RightPane from './components/RightPane';
 import { SampleProvider } from './contexts/sample';
+import { AnotherProvider } from './contexts/another';
+
+const AppProvider = ({ contexts, children }) => contexts.reduce(
+    (prev, context) => React.createElement(context, {
+        children: prev
+    }),
+    children
+);
 
 const App = () => {
     return (
-        <SampleProvider>
+        <AppProvider
+            contexts={[SampleProvider, AnotherProvider]}
+        >
             <div className="panes">
                 <LeftPane />
                 <RightPane />
             </div>
-        </SampleProvider>
+        </AppProvider>
     );
 };
 
